88b1b9f6aba5510781ef858f3e1490b0c3c1ef52
anitatechnical97 <anitatechnical97@gmail.com>
2023-06-19 00:59:04 +0530

    function and nested function calling lecture 56 batch b1 2021

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// 1. Function definition
+function myFunctionA(t) {
+    console.log('The message is :-');
+    // Function body
+  }
+  
+  //  Function Calling
+    myFunctionA('A');         // String
+    var a = 'A '
+    var b = "Flower ";
+    var c = `needs `;
+    var d = 'water,';
+    console.log(a+b+c+d);
+  
+    let e = "A ";
+    let f = 'tree ';
+    let g = `needs `;
+    let h = 'water,'
+    console.log(e+f+g+h);
+
+    var i = "A ";
+    var j = 'rabbit ';
+    var k = `needs `;
+    var l = 'water,';
+    console.log(i+j+k+l);
+
+    var m = "A ";
+    var n = 'horse ';
+    var o = `needs `;
+    var p = "water,";
+    console.log(m+n+o+p);
+
+    var q = "You ";
+    var r = 'need ';
+    var s = `water,`;
+    console.log(q+r+s);
+
+    var t = "I ";
+    var u = 'need ';
+    var v = `water,`;
+    console.log(t+u+v);
+
+    var w = "We ";
+    var x = 'all ';
+    var y = `need `;
+    var z = "water!";
+    console.log(w+x+y+z);
+  
+    
